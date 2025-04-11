6255207feae53ca9d2b3ced5665cf02b1bdd3582
keverson-ms <keverson.marquesouza@gmail.com>
2025-04-11 16:07:07 -0400

    Exercício de fixação de iteração com múltiplas listas

---
+++ aprendendo_python_conceitos_basicos/exercicios/sequencias_e_loops/desafio-2-iteracao-com-multiplas-listas.py
@@ -0,0 +1,32 @@
+print('1) Dado duas listas, print todos os valores que aparecerem duplicados nas duas listas.\n')
+
+lista1 = list(range(0, 26, 2))
+lista2 = list(range(0, 26, 3))
+duplicados = list()
+
+for l1 in lista1:
+   for l2 in lista2:
+       if l1 == l2:
+           duplicados.append(l1)
+           print(f'O valor {l1} está presente nas duas listagens')
+
+print(f'\nEstá lista {duplicados} contém valores presentes na listagem 1 e 2\n')
+print('-'*len(duplicados)+' Fim do exercício 1 '+'-'*len(duplicados))
+
+print('\n2) Dado duas listas, print uma mensagem dizendo se existe algum elemento em comum entre elas ou não.\n')
+
+elemento_comum = False
+
+for l1 in lista1:
+   for l2 in lista2:
+       if l1 == l2:
+           elemento_comum = True
+
+if elemento_comum :
+    print(f'As listas {lista1} e {lista2} possuem elementos em comum')
+else:
+        print(f'As listas {lista1} e {lista2} NÃO possuem elementos em comum')
+
+print('-'*len(lista1)+' Fim do exercício 2 '+'-'*len(lista1))
+
+
