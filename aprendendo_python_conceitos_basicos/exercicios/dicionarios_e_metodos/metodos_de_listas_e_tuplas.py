09609b85045ef3a66d96a9ac9979d63eb22b8e5e
keverson-ms <keverson.marquesouza@gmail.com>
2025-05-20 15:44:25 -0400

    *utlizando métodos de listas e tuplas;

---
+++ aprendendo_python_conceitos_basicos/exercicios/dicionarios_e_metodos/metodos_de_listas_e_tuplas.py
@@ -0,0 +1,15 @@
+tup = (0, 0, 0, 1, 2, 3, 4)
+
+print(tup.index(3))
+
+print(tup.count(3))
+
+l1 = [0, 0, 0, 1, 2, 3, 4]
+l2 = l1.copy()
+
+l1.clear()
+
+for n in range(5):
+    print(l2.append(n * 3))
+
+valores =
